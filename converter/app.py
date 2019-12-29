a7161883a4068426e0d200b831e4e48b14dad6c1
Michael Kim <mkim0407@gmail.com>
2019-12-29 13:38:40 -0800

    parse stellaris save

--- converter/app.py
+++ converter/app.py
@@ -1,8 +1,62 @@
 import json
+from io import BytesIO
+from tokenize import tokenize
+from zipfile import ZipFile
+
+from clausewitz.parse import parse
+from clausewitz.util.tokenize import prepare
+from requests_toolbelt.multipart import decoder
+
+ENCODING = 'utf-8'
+
+
+def get_field_name(content_disposition: bytes):
+    content_disposition = content_disposition.decode(ENCODING)
+    for field in content_disposition.split(';'):
+        field = field.strip()
+        if not field.startswith('name='):
+            continue
+        return field[5:].strip('"')
+    return None
+
+
+def unzip_save(f: BytesIO):
+    with ZipFile(f) as zip:
+        with zip.open('meta') as meta:
+            yield meta
+        with zip.open('gamestate') as gamestate:
+            yield gamestate
+
+
+def jsonify(f):
+    return parse(tokenize(prepare(f.readline)))
 
 
 def lambda_handler(event, context):
+    multipart_data = decoder.MultipartDecoder(
+        event['body'].encode(ENCODING),
+        event['headers']['Content-Type'],
+        encoding=ENCODING,
+    )
+
+    meta = None
+    gamestate = None
+    for part in multipart_data.parts:
+        name = get_field_name(part.headers[b'Content-Disposition'])
+        with BytesIO(part.content) as f:
+            if name == 'meta':
+                meta = jsonify(f)
+            elif name == 'gamestate':
+                gamestate = jsonify(f)
+            elif name == 'file':
+                unzipped = unzip_save(f)
+                meta = jsonify(next(unzipped))
+                gamestate = jsonify(next(unzipped))
+
     return {
         "statusCode": 200,
-        "body": json.dumps(event),
+        "body": json.dumps({
+            'meta': meta,
+            'gamestate': gamestate,
+        }),
     }
