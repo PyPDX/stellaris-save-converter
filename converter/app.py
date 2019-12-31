237d614e01c3c1f1054d0ad4e508fae1a6c16f1f
Michael Kim <mkim0407@gmail.com>
2019-12-31 01:43:54 -0800

    rewrite converter to interface with s3

--- converter/app.py
+++ converter/app.py
@@ -1,65 +1,88 @@
 import json
+import logging
+import os
+from contextlib import contextmanager
 from io import BytesIO
 from tokenize import tokenize
 from zipfile import ZipFile
 
+import boto3
 from clausewitz.parse import parse
 from clausewitz.util.tokenize import prepare
-from requests_toolbelt.multipart import decoder
 
 ENCODING = 'utf-8'
+s3 = boto3.client('s3')
+BUCKET_NAME = os.environ['BUCKET_NAME']
+EXPIRATION = int(os.environ.get('EXPIRATION', 60))  # default: 1 min
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
-def get_field_name(content_disposition: bytes):
-    content_disposition = content_disposition.decode(ENCODING)
-    for field in content_disposition.split(';'):
-        field = field.strip()
-        if not field.startswith('name='):
-            continue
-        return field[5:].strip('"')
-    return None
+@contextmanager
+def get_s3_file(key) -> BytesIO:
+    logger.info(f"Downloading '{key}' to memory")
+    with BytesIO() as f:
+        s3.download_fileobj(BUCKET_NAME, key, f)
+        f.seek(0)
+        yield f
 
 
 def unzip_save(f: BytesIO):
     with ZipFile(f) as zip:
         with zip.open('meta') as meta:
-            yield meta
-        with zip.open('gamestate') as gamestate:
-            yield gamestate
+            logger.info('Parsing meta')
+            yield 'meta', meta
+        # TODO this takes too long..
+        # with zip.open('gamestate') as gamestate:
+        #     logger.info('Parsing gamestate')
+        #     yield 'gamestate', gamestate
 
 
 def jsonify(f):
     return parse(tokenize(prepare(f.readline)))
 
 
-def lambda_handler(event, context):
-    headers = event['headers']
-    try:
-        content_type = headers['Content-Type']
-    except KeyError:
-        content_type = headers['content-type']
+def upload(key, data):
+    binary_data = json.dumps(
+        data,
+        separators=(',', ':'),
+    ).encode(ENCODING)
+    logger.info(f"Uploading {len(binary_data)} bytes to '{key}'")
 
-    body = event['body'].encode(ENCODING)
+    with BytesIO(binary_data) as f:
+        s3.upload_fileobj(f, BUCKET_NAME, key)
 
-    multipart_data = decoder.MultipartDecoder(
-        body,
-        content_type,
-        encoding=ENCODING,
+
+def presign_download(key):
+    return s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': BUCKET_NAME,
+            'Key': key,
+        },
+        ExpiresIn=EXPIRATION,
     )
 
-    meta = None
-    gamestate = None
-    for part in multipart_data.parts:
-        name = get_field_name(part.headers[b'Content-Disposition'])
-        with BytesIO(part.content) as f:
-            if name == 'meta':
-                meta = jsonify(f)
-            elif name == 'gamestate':
-                gamestate = jsonify(f)
-            elif name == 'file':
-                unzipped = unzip_save(f)
-                meta = jsonify(next(unzipped))
-                gamestate = jsonify(next(unzipped))
+
+def lambda_handler(event, context):
+    data = json.loads(event['body'])
+
+    key = data['key']
+    with get_s3_file(key) as f:
+        result = {
+            name: jsonify(unzipped)
+            for name, unzipped in unzip_save(f)
+        }
+
+    try:
+        save_name = result['meta']['name'].strip('"') + ' - ' + result['meta']['date'].strip('"')
+    except KeyError:
+        save_name = 'converted'
+
+    new_key = f"{os.path.dirname(key)}/{save_name}.json"
+    upload(new_key, result)
+    download_url = presign_download(new_key)
 
     return {
         'statusCode': 200,
@@ -69,7 +92,6 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Methods': 'POST',
         },
         'body': json.dumps({
-            'meta': meta,
-            'gamestate': gamestate,
+            'url': download_url,
         }),
     }
