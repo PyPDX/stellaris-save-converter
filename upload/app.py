598fbf8a90b9e3eb7e476988c86374e750506023
Michael Kim <mkim0407@gmail.com>
2019-12-30 23:58:11 -0800

    implement presign url

--- upload/app.py
+++ upload/app.py
@@ -1,8 +1,30 @@
 import json
+import os
+import uuid
+from datetime import datetime
+
+import boto3
+
+s3 = boto3.client('s3')
+BUCKET_NAME = os.environ['BUCKET_NAME']
+EXPIRATION = int(os.environ.get('EXPIRATION', 60))  # default: 1 min
+
+
+def presign_upload(key):
+    return s3.generate_presigned_post(
+        BUCKET_NAME,
+        key,
+        ExpiresIn=EXPIRATION,
+    )
 
 
 def lambda_handler(event, context):
+    now = datetime.now()
+    key = f'{now.year}/{now.month}/{now.day}/{uuid.uuid4()}/save.zip'
+
+    response = presign_upload(key)
+
     return {
         'statusCode': 200,
-        'body': json.dumps(event),
+        'body': json.dumps(response),
     }
